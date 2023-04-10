a858a6cb2e90a87d3f21fcb83cf8c26267b28164
progforyou <97514985+progforyou@users.noreply.github.com>
2023-04-10 20:58:19 +0700

    Update CreateUser.js

--- src/components/Modal/CreateUser.js
+++ src/components/Modal/CreateUser.js
@@ -173,7 +173,6 @@ export const CreateUserMW = (props) => {
                                         className="w-full border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
                                     <option value={"1"}>Пользователь</option>
                                     <option value="2">Админ</option>
-                                    <option value="3">Владелец</option>
                                 </select>
                                 {error.length > 0 &&
                                     <span className='text-sm mt-1' style={{color: "red"}}>{error}</span>}
